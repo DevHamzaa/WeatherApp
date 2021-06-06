3d0e72deb5f6e7b73109508bc896a038a02c4105
Dev Hamza <dev.hamzaa@gmail.com>
2021-06-06 15:13:00 +0500

    ExpressJs Weather App

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+// ...................
+// Global Varriables etc here
+const app = express();
+const port = process.env.PORT || 3000;
+const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+// ..................
+app.set('view engine', 'hbs');
+app.set('views', templatePath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(staticPath));
+app.get('/', (req, res) => {
+	req;
+	res.render('index');
+});
+app.get('/about', (req, res) => {
+	req;
+	res.render('about');
+});
+app.get('/weather', (req, res) => {
+	req;
+	res.render('weather');
+});
+app.get('*', (req, res) => {
+	req;
+	res.render('404page');
+});
+
+// Listning to port
+app.listen(port, () => {
+	console.log('Listning to port 3000');
+});
