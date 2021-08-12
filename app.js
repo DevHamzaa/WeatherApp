d34daa511f1e6a2bf8019ccba5d4cfa15b71b639
Dev Hamza <dev.hamzaa@gmail.com>
2021-08-12 01:47:12 -0700

    heroku

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const hbs = require("hbs");
 // Global Varriables etc here
 const app = express();
 const port = process.env.PORT || 3000;
-const staticPath = path.join(__dirname, "../public");
-const templatePath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const staticPath = path.join(__dirname, "./public");
+const templatePath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
 // ..................
 app.set("view engine", "hbs");
 app.set("views", templatePath);
